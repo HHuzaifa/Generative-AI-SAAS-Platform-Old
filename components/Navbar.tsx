39b71ee123beb76dcc1878536da078ebf14cd8f1
HHuzaifa <huzaifa50100@gmail.com>
2023-07-27 01:17:40 +0500

    .

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,21 @@
-import { Button } from "./ui/button";
-import { Menu } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
 
-const Navbar = () => {
-  return (
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
+
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription();
+
+  return ( 
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default Navbar;
