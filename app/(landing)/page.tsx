10b6339bd7fb9da19346eec0bde1c2b292734343
HHuzaifa <huzaifa50100@gmail.com>
2023-07-27 08:56:11 +0500

    .

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,15 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
+
 const LandingPage = () => {
-    return (
-    <section>
-        Landing Page (Unprotected)
-    </section>
-  );
-
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+   );
 }
-
+ 
 export default LandingPage;
